cb5a52981ed7e6bd710dd8c5fc67cea3bc210cf7
Chiman Mandali <mandali1988@gmail.com>
2023-12-08 16:21:55 +0530

    Add whole Task

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*"],
+  theme: {
+    extend: {},
+    fontFamily :{ 
+      Metropolis: ["Metropolis", "sans-serif"],  
+    } 
+  },
+  plugins: [],
+}
+
